3f11a30ebbfc5ccef6530b1af2eaba3bc2a475f5
Jojo <jojothomas1515@gmail.com>
2023-03-08 23:49:08 +0100

    all console methods implemented

--- console.py
+++ console.py
@@ -24,7 +24,9 @@ class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb)"
     model_dict: dict = {"BaseModel": BaseModel}
 
-    # validator = re.compile(r"([A-Z][^\.]+)\.([\w-]+)")
+    def emptyline(self):
+        """Does Nothing."""
+        pass
 
     def do_quit(self, line):
         """Exit the command line."""
@@ -43,7 +45,9 @@ class HBNBCommand(cmd.Cmd):
         if line == "":
             print("** class name missing **")
         elif line in self.model_dict.keys():
-            obj = self.model_dict[line]().save()
+            obj = self.model_dict[line]()
+            obj.save()
+            print(obj.id)
         else:
             print("** class doesn't exist **")
 
@@ -57,11 +61,13 @@ class HBNBCommand(cmd.Cmd):
 
         if line == "":
             print("** class name missing **")
-        elif line.split(".")[0] in self.model_dict.keys():
-            if len(line.split(".")) == 2:
+        elif line.split(" ")[0] in self.model_dict.keys():
+            if len(line.split(" ")) == 2:
                 result = storage.all()
-                if line in result.keys():
-                    print(result[line])
+                key = ".".join((line.split(" ")[0],
+                                line.split(" ")[1]))
+                if key in result.keys():
+                    print(result[key])
                 else:
                     print("** no instance found **")
             else:
@@ -72,17 +78,20 @@ class HBNBCommand(cmd.Cmd):
     def do_destroy(self, line):
         """Deletes an instance based on the class name and id
          (save the change into the JSON file). Ex: $ destroy BaseModel 1234-1234-1234.
-
+ key = ".".join((line.split(" ")[0],
+                                line.split(" ")[1]))
          Args:
             line: argument that command is supposed to work
          """
         # Todo:delete instance and save further implementations
         if line == "":
             print("** class name missing **")
-        elif line.split(".")[0] in self.model_dict.keys():
-            if len(line.split(".")) == 2:
+        elif line.split(" ")[0] in self.model_dict.keys():
+            if len(line.split(" ")) == 2:
+                key = ".".join((line.split(" ")[0],
+                                line.split(" ")[1]))
                 try:
-                    del storage._FileStorage__objects[line]
+                    del storage._FileStorage__objects[key]
                     storage.save()
                 except KeyError as e:
                     print("** no instance found **")
@@ -97,24 +106,67 @@ class HBNBCommand(cmd.Cmd):
         """Prints all string representation of all
         instances based or not on the class name.
         Ex: $ all BaseModel or $ all.
+
+        Args:
+            line: command argument
         """
         result: dict
         if line == "":
             result = storage.all()
-            for k, v in result.items():
-                print(BaseModel(**v))
+            for key, value in result.items():
+                print(value)
         elif line in self.model_dict.keys():
             result = storage.all()
-            for k,v in result.items():
+            for k, v in result.items():
                 if k.split(".")[0] == line:
-                    print(BaseModel(**v))
+                    print(v)
         else:
             print("** class doesn't exist **")
 
+    def do_update(self, line):
+        """ Updates an instance based on the class name and id by adding
+        or updating attribute (save the change into the JSON file).
 
-def emptyline(self):
-    """Does Nothing."""
-    pass
+        Example:
+            update BaseModel 1234-1234-1234 email "aibnb@mail.com".
+        Args:
+            line: command argument
+        """
+        reg_text = r"(?P<model>{})?.?" \
+                       .format("|".join(self.model_dict.keys())) + \
+                   r"(?P<id>[a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}" \
+                   r"-[a-z0-9]{4}-[a-z0-9]{12})?.?" \
+                   r"(?P<key>[\w\d_]+)?.?" \
+                   r"(?P<value>\"[^\"]+\"|[^ ]+)?"
+        validator = re.compile(reg_text)
+        res = validator.search(line)
+        _model, _id, _key, _value = res.groups()
+        if line == "":
+            print("** class name missing **")
+        elif _model in self.model_dict.keys():
+            if _id:
+                if _key:
+                    if _value:
+                        # Todo: implement update
+                        if _key not in ["created_at", 'updated_at', "id"]:
+                            print(res.groups())
+                            inst = ".".join((_model,
+                                             _id))
+                            try:
+                                obj = storage.all()[inst]
+                                obj.__dict__[_key] = eval(_value)
+                                storage.save()
+                            except KeyError:
+                                print("** no instance found **")
+                    else:
+                        print("** attribute name missing **")
+                else:
+                    print("** attribute name missing **")
+            else:
+                print("** instance id missing **")
+        else:
+            print("** class doesn't exist **")
+
 
 banner = """
    (_)         (_)      (_)(_)(_)(_) _       (_) _       (_)      (_)(_)(_)(_) _    
@@ -126,8 +178,7 @@ banner = """
    (_)         (_)      (_)(_)(_)(_)         (_)         (_)      (_)(_)(_)(_)      
 """
 intro = "{}\t{}\n\tconsole By JOJO THOMAS and VICTORIA OLABODEH{}\n" \
-        "\n{}\tW\tE\tL\tC\tO\tM\tE\n\n{}".format(TermColor.OKCYAN,banner,TermColor.ENDC,
+        "\n{}\tW\tE\tL\tC\tO\tM\tE\n\n{}".format(TermColor.OKCYAN, banner, TermColor.ENDC,
                                                  TermColor.HEADER, TermColor.ENDC)
-
 if __name__ == '__main__':
     HBNBCommand().cmdloop(intro=intro)
